69e59eae0c32384a922583fe555c30f302687da7
Dai Wei <nbdavied@gmail.com>
2018-09-25 17:16:36 +0800

    增加刷新access token 接口

--- src/main/java/com/dw/exercise/controller/AuthController.java
+++ src/main/java/com/dw/exercise/controller/AuthController.java
@@ -1,5 +1,10 @@
 package com.dw.exercise.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.dw.exercise.dao.UserDAO;
 import com.dw.exercise.entity.User;
 import com.dw.exercise.service.AuthService;
 import com.dw.exercise.vo.AuthUser;
@@ -9,15 +14,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.dw.exercise.security.SecurityConstants.EXPIRATION_TIME;
+import static com.dw.exercise.security.SecurityConstants.SECRET;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
     AuthService authService;
+    @Resource
+    UserDAO userDAO;
+
     @RequestMapping("/signup")
     public User signup(@RequestBody AuthUser authUser, HttpServletResponse response){
         return authService.singUp(authUser);
@@ -28,6 +42,27 @@ public class AuthController {
         String token = authService.signIn(authUser.getUsername(), authUser.getPassword());
         response.addHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8");
         String result = "{\"token\":\"" + token+ "\"}";
+        PrintWriter writer = response.getWriter();
+        writer.write(result);
+        writer.flush();
+        writer.close();
+    }
+    @RequestMapping("/refresh")
+    public void refreshToken(String token, HttpServletResponse response) throws IOException {
+        DecodedJWT jwt =  JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                    .build()
+                    .verify(token);
+        String username = jwt.getSubject();
+        User user = userDAO.getUserByUsername(username);
+        String newtoken = JWT.create()
+                .withSubject(username)
+                .withClaim("uid", user.getId())
+                .withClaim("nic", user.getNickname())
+                .withArrayClaim("rol",  user.getRoles().toArray(new String[0]))
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(SECRET.getBytes()));
+        response.addHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8");
+        String result = "{\"token\":\"" + newtoken+ "\"}";
         PrintWriter writer = response.getWriter();
         writer.write(result);
         writer.flush();
